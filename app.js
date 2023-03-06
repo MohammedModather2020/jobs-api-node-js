04d3fa8de92d3dbff0687ca4033b46a4ea8f11b9
MohammedModather2020 <mohammedmodather144@gamil.com>
2023-03-06 19:10:31 +0200

    Add not found middleware

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 
 // error handler
-const notFoundMiddleware = require('./middleware/not-found');
+const notFoundMiddleware = require('./middleware/not-found.middleware');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());
