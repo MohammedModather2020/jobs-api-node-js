7811008e1ea663fcf1ee94aef3dcb530fa81cb05
MohammedModather2020 <mohammedmodather144@gamil.com>
2023-03-07 17:36:21 +0200

    Create setup job controller

---
+++ controllers/job.controller.js
@@ -0,0 +1,27 @@
+const getJobs = async (req, res) => {
+  res.send('Get all jobs');
+};
+
+const getJob = async (req, res) => {
+  res.send('Get single job');
+};
+
+const createJob = async (req, res) => {
+  res.send('Create new job');
+};
+
+const updateJob = async (req, res) => {
+  res.send('Update job');
+};
+
+const deleteJob = async (req, res) => {
+  res.send('Delete job');
+};
+
+module.exports = {
+  getJobs,
+  getJob,
+  createJob,
+  updateJob,
+  deleteJob,
+};
